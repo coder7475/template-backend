c5c8a5f8c2b6cb5e2186b06df35ae296a4d24890
coder7475 <robiulhossain7475@gmail.com>
2024-06-07 20:13:55 +0600

    feat: add cors origin

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import notFound from './middlewares/notFound';
+import parameters from './parameters';
 const app: Application = express();
 
 // *? middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:7475'],
+    credentials: parameters.env === 'production' ? true : false,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  }),
+);
 
 //? For all others routes - Routes Not Found
 app.use(notFound);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 import app from './app';
-import config from './config';
+import parameters from './parameters';
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(parameters.database_url as string);
     console.log('connected to mongodb...');
-    app.listen(config.port, () => {
-      console.log(`App is listening on port ${config.port}`);
+    app.listen(parameters.port, () => {
+      console.log(`App is listening on port ${parameters.port}`);
     });
   } catch (err) {
     console.log(err);
