82be9edfd77de26c233966eb963a55a8aba6bce5
coder7475 <robiulhossain7475@gmail.com>
2024-06-07 20:35:32 +0600

    feat: use catch async for notFound handler

--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express';
 import sendResponse from '../utils/sendResponse';
 import httpStatus from 'http-status';
+import catchAsync from '../utils/catchAsync';
 
-const notFound = (req: Request, res: Response) => {
+const notFound = catchAsync((req: Request, res: Response) => {
   sendResponse(res, {
     statusCode: httpStatus.NOT_FOUND,
     success: false,
     message: 'NOT FOUND',
     data: null,
   });
-};
+});
 
 export default notFound;
--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
 // * Higher order function to take route callback function and handle error
-// * Replaces try {} catch block
+// * Replaces try {} catch block - handle promise
 const catchAsync = (fn: RequestHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((err) => next(err));
