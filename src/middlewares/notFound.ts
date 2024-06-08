a5f64c15010d47712411eea951226b7aa2514eaf
coder7475 <robiulhossain7475@gmail.com>
2024-06-08 19:24:47 +0600

    feat: add next to notFound

--- src/middlewares/notFound.ts
+++ src/middlewares/notFound.ts
@@ -1,15 +1,18 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import sendResponse from '../utils/sendResponse';
 import httpStatus from 'http-status';
 import catchAsync from '../utils/catchAsync';
 
-const notFound = catchAsync((req: Request, res: Response) => {
-  sendResponse(res, {
-    statusCode: httpStatus.NOT_FOUND,
-    success: false,
-    message: 'NOT FOUND',
-    data: null,
-  });
-});
+const notFound = catchAsync(
+  (req: Request, res: Response, next: NextFunction) => {
+    sendResponse(res, {
+      statusCode: httpStatus.NOT_FOUND,
+      success: false,
+      message: 'NOT FOUND',
+      data: null,
+    });
+    next();
+  },
+);
 
 export default notFound;
