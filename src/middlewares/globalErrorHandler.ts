df12d95396904335b7f4e5172a8099078f224ec8
coder7475 <robiulhossain7475@gmail.com>
2024-06-08 19:48:01 +0600

    feat: create the baisic structure of global error handler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,13 +1,26 @@
 import { ErrorRequestHandler } from 'express';
 import { TErrorSources } from '../interfaces/error';
+import parameters from '../parameters';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  let statusCode = 500;
-  let message = 'Global Error Occured!';
-  let errorSources: TErrorSources = [
+  // ser default error message values
+  const statusCode = 500;
+  const message = 'Global Error Occured!';
+  const errorSources: TErrorSources = [
     {
       path: '',
       message: 'Find Error in Chaos!',
     },
   ];
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    err,
+    stack: parameters.env === 'development' ? err?.stack : null,
+  });
+  next();
 };
+
+export default globalErrorHandler;
