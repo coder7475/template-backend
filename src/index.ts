22b83f1fab2a7d059feff6f86786650ddb265a8c
coder7475 <robiul7475@gmail.com>
2024-06-14 22:08:46 +0600

    feat: install prettier-eslint for the vscode prettier-eslint extension

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,36 @@ import app from './app';
 import { Server } from 'http';
 import mongoose from 'mongoose';
 import parameters from './parameters';
-
 // declare server variable
 let server: Server;
-
 async function main() {
-  try {
-    // Connect to mongodb
-    await mongoose.connect(parameters.database_url as string);
-    console.log('connected to mongodb...');
-    // create server
-    server = app.listen(parameters.port, () => {
-      console.log(`App is listening on port ${parameters.port}`);
-    });
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        // Connect to mongodb
+        await mongoose.connect(parameters.database_url as string);
+        console.log('connected to mongodb...');
+        // create server
+        server = app.listen(parameters.port, () => {
+            console.log(`App is listening on port ${parameters.port}`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 // ------------MAIN-----------------
 main(); // * run the main function
 //-----------------------------------
 //? handle special errors
 process.on('unhandledRejection', () => {
-  console.log(`😈 unahandledRejection is detected , shutting down ...`);
-  if (server) {
-    server.close(() => {
-      process.exit(1);
-    });
-  }
-  process.exit(1);
+    console.log(`😈 unahandledRejection is detected , shutting down ...`);
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
 });
 
 process.on('uncaughtException', () => {
-  console.log(`😈 uncaughtException is detected , shutting down ...`);
-  process.exit(1);
+    console.log(`😈 uncaughtException is detected , shutting down ...`);
+    process.exit(1);
 });
